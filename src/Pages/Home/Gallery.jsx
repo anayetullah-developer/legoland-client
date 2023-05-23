4a0a711fbe7e09db1f4ec036d0906603d896e561
anayet ullah <anayetdev@gmail.com>
2023-05-23 23:55:30 +0600

    Finally revised

--- src/Pages/Home/Gallery.jsx
+++ src/Pages/Home/Gallery.jsx
@@ -3,7 +3,11 @@ import { Card, CardGroup } from "react-bootstrap";
 import img1 from "../../assets/banner/1.jpg";
 import img2 from "../../assets/banner/2.jpg";
 import img3 from "../../assets/banner/3.jpg";
-import img4 from "../../assets/banner/3 (4).jpg";
+import img4 from "../../assets/banner/4.jpg";
+import img5 from "../../assets/banner/5.jpg";
+import img6 from "../../assets/banner/6.jpg";
+import img7 from "../../assets/banner/7.jpg";
+import img8 from "../../assets/banner/8.jpg";
 
 
 const Gallery = () => {
@@ -26,16 +30,16 @@ const Gallery = () => {
       </CardGroup>
       <CardGroup>
         <Card className="img-hover">
-          <Card.Img variant="top" src={img1} />
+          <Card.Img variant="top" src={img5} />
         </Card>
         <Card className="img-hover">
-          <Card.Img variant="top" src={img2} />
+          <Card.Img variant="top" src={img6} />
         </Card>
         <Card className="img-hover">
-          <Card.Img variant="top" src={img3} />
+          <Card.Img variant="top" src={img7} />
         </Card>
         <Card className="img-hover">
-          <Card.Img variant="top" src={img4} />
+          <Card.Img variant="top" src={img8} />
         </Card>
       </CardGroup>
     </div>
