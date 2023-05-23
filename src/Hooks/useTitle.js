1989121b65260d0696afaaee3a4ec14411e4b146
anayet ullah <anayetdev@gmail.com>
2023-05-23 23:47:08 +0600

    Dynamic title issue solved

--- src/Hooks/useTitle.js
+++ src/Hooks/useTitle.js
@@ -2,7 +2,7 @@ import { useEffect } from "react"
 
 const useTitle = title => {
     useEffect(()=>{
-        document.title = `Legoland - ${title}`
+        document.title = `${title} | Legoland`
     },[title])
 }
 
